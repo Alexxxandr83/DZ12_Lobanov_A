8698e336111d2ba4bbba257adfe09355471045b8
Alexandr L <alexandp83@mail.ru>
2023-03-26 21:59:38 +0300

    add func title_letter

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def upper_letter(text):
+    """ф-ция принимает на вход строку и возвращает ее со всеми заглавными буквами"""
     output = text.upper()
+    return output
+
+def title_letter(text):
+    """ф-ция делает заглавными первые буквы каждого слова в строке, поступившей на вход функции"""
+    output = text/title()
     return output
